d914e5900919d52d1ccf67436eb641e513b3df6e
Jaime Oliveira <jaime.amo18@gmail.com>
2020-12-29 15:09:51 +0000

    ci(GH-Pages): Rebuild pages at 2a118a1

--- src/app/simple-player/simple-player.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-simple-player',
-    templateUrl: './simple-player.component.html',
-    styleUrls: [ './simple-player.component.css' ]
-})
-export class SimplePlayerComponent {
-}
