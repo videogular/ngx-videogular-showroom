7aa610fa6cb8640e74d508d0051a344082dfcdbe
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2021-01-06 14:39:40 +0100

    one last try

--- src/app/single-player/single-player.component.ts
+++ src/app/single-player/single-player.component.ts
@@ -12,15 +12,15 @@ export class SinglePlayerComponent {
     constructor() {
         this.sources = [
             {
-                src: 'http://static.videogular.com/assets/videos/videogular.mp4',
+                src: 'https://cors-anywhere.herokuapp.com/http://static.videogular.com/assets/videos/videogular.mp4',
                 type: 'video/mp4'
             },
             {
-                src: 'http://static.videogular.com/assets/videos/videogular.ogg',
+                src: 'https://cors-anywhere.herokuapp.com/http://static.videogular.com/assets/videos/videogular.ogg',
                 type: 'video/ogg'
             },
             {
-                src: 'http://static.videogular.com/assets/videos/videogular.webm',
+                src: 'https://cors-anywhere.herokuapp.com/http://static.videogular.com/assets/videos/videogular.webm',
                 type: 'video/webm'
             }
         ];
